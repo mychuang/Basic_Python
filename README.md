2e64ece4ea7a615ff9be698d192d9b00abd13d61
Yi-Cheng Huang <hhjoy222@gmail.com>
2023-01-26 22:07:08 +0800

    輸入與強制轉型

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Basic_Python
+
+- ch01: First Python code
+
+- ch02: 變數初探
+
+- ch03: 
---
+++ ch02/3.py
@@ -0,0 +1,28 @@
+#輸入與輸出
+
+a = input("Input an integer: ")
+print(a)
+print(type(a))
+
+b = input("Input a string: ")
+print(b)
+print(type(b))
+
+c = input("Input a float: ")
+print(c)
+print(type(c))
+
+# 使用強制轉型
+
+a = int(input("Input an integer: "))
+print(a)
+print(type(a))
+
+b = input("Input a string: ")
+print(b)
+print(type(b))
+
+c = float(input("Input a float: "))
+print(c)
+print(type(c))
+
