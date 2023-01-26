56a4a56a939d5cfe1a2cf4346439cc9d81f19d44
Yi-Cheng Huang <hhjoy222@gmail.com>
2023-01-26 21:59:58 +0800

    first commit

---
+++ ch02/1.py
@@ -0,0 +1,26 @@
+# 變數宣告初探
+
+# integer
+a = 10
+print(a)
+print(type(a))
+
+# float
+b = 10.5
+print(b)
+print(type(b))
+
+# string
+c = "Hello world"
+print(c)
+print(type(c))
+
+# bool
+d = True
+print(d)
+d = False
+print(d)
+print(type(d))
+
+
+
---
+++ ch02/2.py
@@ -0,0 +1,12 @@
+# 變數宣告: 記憶體控制
+
+# integer
+a = 10
+print(id(a))
+
+b = 10
+print(id(b))
+
+a = 9
+print(id(a))
+
